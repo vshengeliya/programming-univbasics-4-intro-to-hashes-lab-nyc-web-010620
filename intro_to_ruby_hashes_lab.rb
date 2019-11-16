c4f9291c615d5079d79cda260a976c04921a190f
bright-format-7059 <v.shengeliya@gmail.com>
2019-11-16 02:28:11 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,29 +1,38 @@
-def new_hash
-  # return an empty hash
+def new_hash={}
 end
 
-def my_hash
-  # return a valid hash with any key/value pair of your choice
+def my_hash=
+  { object: = "glass"}
 end
 
-def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+def pioneer=
+  {:name => "Grace Hopper"}
 end
 
-def id_generator
-  # return a hash with a key :id assigned to positive integer
+def id_generator =
+  {:id =>54321}
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+   = {:name => "Grace Hopper"}
 end
 
-def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+def  = {:soap => 2, :shampoo =>1, :toothpaste => 2}
+   if my_hash_creator[:showergel]
+  puts "Showergel included!"
+else
+  puts " No showergel"
+  end
 end
 
-def update_counting_hash(hash, key)
-  # given a hash an a key as parameters, return an updated hash
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
+def update_counting_hash(hash, key) = 
+  {:pears => 2, :apples =>3, :strawberries =>5 }
+  if update_counting_hash ["apples"]
+     update_counting_hash ["apples"] +=1
+   end 
+   if update_counting_hash ["orangies"]
+     update_counting_hash ["orangies"] +=1
+     else 
+     update_counting_hash ["orangies"] = 1
+     end   
 end
