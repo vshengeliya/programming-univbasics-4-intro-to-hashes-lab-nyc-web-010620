d4d535e0374e1ff2b700ce18ad1a643837f37286
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-05 05:06:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,18 +18,11 @@ def my_hash_creator(key, value)
    {:key => value}
 end
 
-def my_hash_creator (key, value)
-
+def read_from_hash(hash, key)
+  hash[key]
 end
 
-# def update_counting_hash(hash, key) = 
-#   {:pears => 2, :apples =>3, :strawberries =>5 }
-#   if update_counting_hash ["apples"]
-#     update_counting_hash ["apples"] +=1
-#   end 
-#   if update_counting_hash ["orangies"]
-#     update_counting_hash ["orangies"] +=1
-#     else 
-#     update_counting_hash ["orangies"] = 1
-#     end   
-# end
+def update_counting_hash(hash, key)
+  hash[key] ? hash[key] += 1 : hash[key] = 1
+  hash
+end
