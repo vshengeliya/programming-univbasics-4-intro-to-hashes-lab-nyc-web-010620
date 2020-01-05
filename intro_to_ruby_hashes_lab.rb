9de2b267387d2c12804a1adfd1b6d7191fc802be
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-05 05:07:30 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,38 +1,28 @@
-def new_hash={}
+def new_hash
+  {}
 end
 
-def my_hash=
-  { object: = "glass"}
+def my_hash
+  {:apples =>3}
 end
 
-def pioneer=
+def pioneer
   {:name => "Grace Hopper"}
 end
 
-def id_generator =
+def id_generator 
   {:id =>54321}
 end
 
 def my_hash_creator(key, value)
-   = {:name => "Grace Hopper"}
+   {key => value}
 end
 
-def  = {:soap => 2, :shampoo =>1, :toothpaste => 2}
-   if my_hash_creator[:showergel]
-  puts "Showergel included!"
-else
-  puts " No showergel"
-  end
+def read_from_hash(hash, key)
+  hash[key]
 end
 
-def update_counting_hash(hash, key) = 
-  {:pears => 2, :apples =>3, :strawberries =>5 }
-  if update_counting_hash ["apples"]
-     update_counting_hash ["apples"] +=1
-   end 
-   if update_counting_hash ["orangies"]
-     update_counting_hash ["orangies"] +=1
-     else 
-     update_counting_hash ["orangies"] = 1
-     end   
+def update_counting_hash(hash, key)
+  hash[key] ? hash[key] += 1 : hash[key] = 1
+  hash
 end
