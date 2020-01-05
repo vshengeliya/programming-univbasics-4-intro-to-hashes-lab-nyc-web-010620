5e5bff9f90467238571b5d5cd5b341fd0799a8ee
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-05 04:52:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,38 +1,38 @@
 def new_hash={}
 end
 
-def my_hash=
-  { object: = "glass"}
-end
+# def my_hash=
+#   { object: = "glass"}
+# end
 
-def pioneer=
-  {:name => "Grace Hopper"}
-end
+# def pioneer=
+#   {:name => "Grace Hopper"}
+# end
 
-def id_generator =
-  {:id =>54321}
-end
+# def id_generator =
+#   {:id =>54321}
+# end
 
-def my_hash_creator(key, value)
-   = {:name => "Grace Hopper"}
-end
+# def my_hash_creator(key, value)
+#   = {:name => "Grace Hopper"}
+# end
 
-def  = {:soap => 2, :shampoo =>1, :toothpaste => 2}
-   if my_hash_creator[:showergel]
-  puts "Showergel included!"
-else
-  puts " No showergel"
-  end
-end
+# def  = {:soap => 2, :shampoo =>1, :toothpaste => 2}
+#   if my_hash_creator[:showergel]
+#   puts "Showergel included!"
+# else
+#   puts " No showergel"
+#   end
+# end
 
-def update_counting_hash(hash, key) = 
-  {:pears => 2, :apples =>3, :strawberries =>5 }
-  if update_counting_hash ["apples"]
-     update_counting_hash ["apples"] +=1
-   end 
-   if update_counting_hash ["orangies"]
-     update_counting_hash ["orangies"] +=1
-     else 
-     update_counting_hash ["orangies"] = 1
-     end   
-end
+# def update_counting_hash(hash, key) = 
+#   {:pears => 2, :apples =>3, :strawberries =>5 }
+#   if update_counting_hash ["apples"]
+#     update_counting_hash ["apples"] +=1
+#   end 
+#   if update_counting_hash ["orangies"]
+#     update_counting_hash ["orangies"] +=1
+#     else 
+#     update_counting_hash ["orangies"] = 1
+#     end   
+# end
